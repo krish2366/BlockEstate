e61bfee19f26448ddfa78b008a8ea4113450bc22
Krish2366 <krish236630@gmail.com>
2025-04-15 20:03:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+import LoginForm from './pages/LoginForm'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <LoginForm/>
+    </>
+  )
+}
+
+export default App
