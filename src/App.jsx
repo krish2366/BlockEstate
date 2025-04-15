0512878d717080334c1523a1b21b89d893884a36
sujal-17-nagpal <sujaln0609y@gmail.com>
2025-04-15 21:01:54 +0530

    5th commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import './App.css'
 import Navbar from './Components/Navbar'
 import LoginForm from './pages/LoginForm'
 import { BrowserRouter as Router , Routes ,Route } from 'react-router-dom'
+import {Toaster} from "react-hot-toast";
 
 function App() {
 
   return (
     <>
       <Navbar/>
+
+      <Toaster />
       <Router>
         <Routes>
           <Route path='/login' element={<LoginForm/>} />
         </Routes>
       </Router>
 
-      <LoginForm/>
+      {/* <LoginForm/> */}
     </>
   )
 }
